48a4f719711561712ad9e06f3b40661cd01675c6
TJ Hindman <thind131@gmail.com>
2018-12-11 12:44:13 -0500

    finished xword sol

--- xword.py
+++ xword.py
@@ -2,9 +2,20 @@
 # -*- coding: utf-8 -*-
 """Crossword Solver Program"""
 
-__author__ = "???"
+__author__ = "tjhindman"
+
+import re
 
 # YOUR HELPER FUNCTION GOES HERE
+
+
+def xword_sol(word, dic):
+    in_word = word.replace(" ", ".")
+    re_word = re.compile(in_word)
+
+    for w in dic:
+        if len(word) == len(w) and re.match(re_word, w):
+            print w
 
 
 def main():
@@ -15,7 +26,7 @@ def main():
         'Please enter a word to solve.\nUse spaces to signify unknown letters: ').lower()
 
     # YOUR ADDITIONAL CODE HERE
-    raise NotImplementedError('Please complete this')
+    xword_sol(test_word, words)
 
 
 if __name__ == '__main__':
